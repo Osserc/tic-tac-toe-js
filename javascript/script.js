dcb323be5e5345868a8ca161d7e4e1528e192fc7
Stefano Rondelli <rstefanofil@hotmail.com>
2022-07-25 22:19:21 +0200

    Refactor functions into factories and modules

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,55 @@
-let gridSpots = [...Array(9).keys()]
-const gameGrid = document.querySelector(`#game-grid`)
+const displayGrid = document.querySelector(`#game-grid`)
 
-for (let i = 0; i < gridSpots.length; i++) {
-    const singleCell = document.createElement(`div`)
-    singleCell.dataset.index = i
-    singleCell.classList.add(`bg-info`, `border`, `border-primary`, `border-3`, `m-0`, `d-flex`, `align-items-center`, `justify-content-center`, `single-cell`)
-    gameGrid.appendChild(singleCell)
+const playerFactory = (name, symbol) => {
+    return { 
+        name,
+        symbol
+    }
 }
+
+const gameController = (() => {
+    const playerOne = playerFactory("Pippo", "X")
+    const playerTwo = playerFactory("Mak", "O")
+    let currentPlayer = playerOne
+
+    // function to swap players after they played
+    const swapPlayers = () => {
+        if (currentPlayer == playerOne) {
+            currentPlayer = playerTwo
+        } else {
+            currentPlayer = playerOne
+        }
+    }
+
+    return {
+        playerOne,
+        playerTwo,
+        swapPlayers
+    }
+})()
+
+const gameGrid = (() => {
+    const gridSpots = [...Array(9).keys()]
+
+    // function to generate the initial empty grid
+    const generateGrid = () => {
+        for (let i = 0; i < gridSpots.length; i++) {
+            const singleCell = document.createElement(`div`)
+            singleCell.dataset.index = i
+            singleCell.classList.add(`bg-info`, `border`, `border-primary`, `border-3`, `m-0`, `d-flex`, `align-items-center`, `justify-content-center`, `single-cell`)
+            displayGrid.appendChild(singleCell)
+        }
+    }
+
+    // function to clear the grid
+    const resetGrid = () => {
+        displayGrid.replaceChildren()
+    }
+
+    return {
+        generateGrid,
+        resetGrid
+    }
+})()
+
+gameGrid.generateGrid()
